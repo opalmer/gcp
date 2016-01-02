930b2c6545b1dc959df53b356488c7e2ead8c359
Oliver Palmer <oliverpalmer@opalmer.com>
2016-01-02 17:04:01 -0500

    cleaned up processfiles() so it's just calling methods on File{}

--- libs/file/workers.go
+++ libs/file/workers.go
@@ -5,9 +5,7 @@ package files
 // compression and encryption
 
 import (
-	"bytes"
-	"code.google.com/p/lzma"
-	"io"
+	"os"
 )
 
 // Channels - Defines what channels we use to communicate with workers on.
@@ -20,65 +18,34 @@ var channels Channels
 
 func openpaths(channel <-chan string) {
 	for path := range channel {
-		channels.files <- NewFile(path)
+		channels.files <- File{sourcepath: path}
 	}
 }
 
 func processfiles(channel <-chan File) {
 	for file := range channel {
-		log.Debugf(file.source.Name())
-
-		encrypt := file.ShouldEncrypt()
-		compress := file.ShouldEncrypt()
-
-		// We have nothing else to do here in this case.
-		if !compress && !encrypt {
-			file.Done()
-			continue
+		// Open the file(s) for the given input
+		err := file.open()
+		if err != nil {
+			if os.IsNotExist(err) {
+				log.Warningf("%s does not exist", file.sourcepath)
+			}
+			file.clean()
+			log.Fatalf("Failed to open %s (err: %s)", file.sourcepath, err)
 		}
 
-		for {
-			data := make([]byte, file.readsize)
-			bytesRead, err := file.source.Read(data)
-			bytesRead64 := int64(bytesRead)
-
-			if err == io.EOF {
-				break
-			} else if err != nil {
-				log.Fatal(
-					"Failed to read %s (err: %s)", file.source.Name(), err)
-			}
-
-			// It's possible we didn't read as many bytes
-			// from the file as we allocated for `data`.  If this
-			// is the case, resize data so it matches the number
-			// of bytes read.  Otherwise we end up with empty bytes
-			// in the file we're writing to disk.
-			if file.readsize > bytesRead64 {
-				data = append([]byte(nil), data[:bytesRead]...)
-			}
-
-			if compress {
-				var compressed bytes.Buffer
-				lzmaWriter := lzma.NewWriterSizeLevel(
-					&compressed, bytesRead64, lzma.BestCompression)
-				_, err := lzmaWriter.Write(data)
-				lzmaWriter.Close()
-
-				if err != nil {
-					log.Fatalf(
-						"Compression failed for %s (err: %s)",
-						file.source.Name(), err)
-				}
-				data = compressed.Bytes()
-			}
-
-			if encrypt {
-
-			}
-
-			file.tempout.Write(data)
+		err = file.process()
+		if err != nil {
+			file.clean()
+			log.Fatalf("Failed to process %s (err: %s)", file.sourcepath, err)
 		}
-		file.Done()
+
+		err = file.save()
+		if err != nil {
+			file.clean()
+			log.Fatalf("Failed to save %s (err: %s)", file.outpath, err)
+		}
+
+		filesProcessing.Done()
 	}
 }
